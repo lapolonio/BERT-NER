bee5e9193eb5f201a4628f47f3546a9f93baf966
Leonardo Apolonio <Leonardo.Apolonio@oracle.com>
2018-12-31 19:57:04 -0500

    update reshape size to be generic InvalidArgumentError (see above for traceback): assertion failed: [`labels` out of bound] [Condition x < y did not hold element-wise:] [x (confusion_matrix_1/control_dependency:0) = ] [ 19 18 18...] [y (ToInt64_5:0) = ] [13]
    13 was hardcoded for NerProcessor (num_labels)

--- BERT_NER.py
+++ BERT_NER.py
@@ -500,9 +500,9 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
             def metric_fn(per_example_loss, label_ids, logits):
             # def metric_fn(label_ids, logits):
                 predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
-                precision = tf_metrics.precision(label_ids,predictions,13,[1,2,4,5,6,7,8,9],average="macro")
-                recall = tf_metrics.recall(label_ids,predictions,13,[1,2,4,5,6,7,8,9],average="macro")
-                f = tf_metrics.f1(label_ids,predictions,13,[1,2,4,5,6,7,8,9],average="macro")
+                precision = tf_metrics.precision(label_ids,predictions,num_labels,[1,2,4,5,6,7,8,9],average="macro")
+                recall = tf_metrics.recall(label_ids,predictions,num_labels,[1,2,4,5,6,7,8,9],average="macro")
+                f = tf_metrics.f1(label_ids,predictions,num_labels,[1,2,4,5,6,7,8,9],average="macro")
                 #
                 return {
                     "eval_precision":precision,
